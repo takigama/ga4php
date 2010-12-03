7e111bcbc1f31d77c90ffcac65ef3b9599bdb397
takigama <takigama@2b1365a5-5b14-1214-0a5f-45382c3b6e0f>
2010-12-03 03:11:01 +0000

    some proof of concept work on the authserver

--- authserver/authd/authd.php
+++ authserver/authd/authd.php
@@ -1,24 +1,69 @@
 <?php
 
-if(file_exists("config.php")) require_once("config.php")
-else {
+if(file_exists("config.php")) {
+	require_once("config.php");
+} else {
 	// config file doesnt exist, we must abort sensibly
 }
 
 // get out master library for ga4php
 require_once("../lib/lib.php");
 
-
+	
+//exit(0);
 // first we want to fork into the background like all good daemons should
-$pid = pcntl_fork();
+//$pid = pcntl_fork();
+$pid = 0;
 
 if($pid == -1) {
 	
 } else if($pid) {
 	// i am the parent, i shall leave
+	echo "i am a parent, i leave\n";
 	exit(0);
 } else {
-	// i am the child, begin me up
+	
+	
+	/// ok, this is just testing stuff... create queue
+	global $MSG_QUEUE_KEY_ID_SERVER, $MSG_QUEUE_KEY_ID_CLIENT;
+	
+	
+	
+	$cl_queue = msg_get_queue($MSG_QUEUE_KEY_ID_CLIENT, 0666 | 'IPC_CREAT');
+	$sr_queue = msg_get_queue($MSG_QUEUE_KEY_ID_SERVER, 0666 | 'IPC_CREAT');
+
+	$myga = new gaasGA();
+	global $myga;
+	
+	
+	print_r($myga);
+	
+	while(true) {
+		msg_receive($sr_queue, 0, $msg_type, 16384, $msg);
+		echo "Got message $msg_type\n";
+		print_r($msg);
+		switch($msg_type) {
+			case MSG_AUTH_USER:
+				echo "got auth message, $msg\n";
+				$username = $msg["user"];
+				$passcode = $msg["passcode"];
+				global $myga;
+				msg_send($cl_queue, MSG_AUTH_USER, $myga->authenticateUser($username, $passcode));
+				break;
+			case MSG_ADD_USER:
+				echo "add user\n";
+				$username = $msg["username"];
+				global $myga;
+				msg_send($cl_queue, MSG_ADD_USER, $myga->setUser($username));
+				break;
+			case MSG_DELETE_USER:
+				break;
+			default:
+				echo "um??\n";
+				
+		}		
+		echo "Back to wait\n";
+	}	
 }
 
 ?>
---
+++ authserver/lib/authClient.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once("lib.php");
+
+class GAAuthClient {
+	function authUser($username, $passcode) {
+		global $MSG_QUEUE_KEY_ID_SERVER, $MSG_QUEUE_KEY_ID_CLIENT;
+		
+		
+		if(!msg_queue_exists($MSG_QUEUE_KEY_ID_SERVER)) {
+			return false;
+		}
+
+		// TODO we need to setup a client queue sem lock here
+		
+		$cl_queue = msg_get_queue($MSG_QUEUE_KEY_ID_CLIENT);
+		$sr_queue = msg_get_queue($MSG_QUEUE_KEY_ID_SERVER);
+		
+		
+		$message["user"] = $username;
+		$message["passcode"] = $passcode;
+		
+		msg_send($sr_queue, MSG_AUTH_USER, $message, true, true, $msg_err);
+		echo "message sent\n";
+		
+		msg_receive($cl_queue, 0, $msg_type, 16384, $msg);
+		echo "message received?\n";
+		print_r($msg);
+		
+		return false;
+	}
+	
+	function addUser($username) {
+		global $MSG_QUEUE_KEY_ID_SERVER, $MSG_QUEUE_KEY_ID_CLIENT;
+		
+		
+		if(!msg_queue_exists($MSG_QUEUE_KEY_ID_SERVER)) {
+			return false;
+		}
+
+		// TODO we need to setup a client queue sem lock here
+		
+		$cl_queue = msg_get_queue($MSG_QUEUE_KEY_ID_CLIENT);
+		$sr_queue = msg_get_queue($MSG_QUEUE_KEY_ID_SERVER);
+		
+		
+		$message["username"] = $username;
+		
+		msg_send($sr_queue, MSG_ADD_USER, $message, true, true, $msg_err);
+		echo "message sent\n";
+		
+		msg_receive($cl_queue, 0, $msg_type, 16384, $msg);
+		echo "message received?\n";
+		print_r($msg);
+		
+		return false;
+		
+	}
+}
+
+?>
