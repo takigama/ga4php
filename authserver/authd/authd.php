d735d0b7c3c8c3c367a26e22f0aae6d4628d38e5
takigama <takigama@2b1365a5-5b14-1214-0a5f-45382c3b6e0f>
2010-12-03 06:55:30 +0000

    authserver code filling

--- authserver/authd/authd.php
+++ authserver/authd/authd.php
@@ -22,12 +22,7 @@ if($pid == -1) {
 	echo "i am a parent, i leave\n";
 	exit(0);
 } else {
-	
-	
-	/// ok, this is just testing stuff... create queue
 	global $MSG_QUEUE_KEY_ID_SERVER, $MSG_QUEUE_KEY_ID_CLIENT;
-	
-	
 	
 	$cl_queue = msg_get_queue($MSG_QUEUE_KEY_ID_CLIENT, 0666 | 'IPC_CREAT');
 	$sr_queue = msg_get_queue($MSG_QUEUE_KEY_ID_SERVER, 0666 | 'IPC_CREAT');
@@ -42,7 +37,7 @@ if($pid == -1) {
 		msg_receive($sr_queue, 0, $msg_type, 16384, $msg);
 		print_r($msg);
 		switch($msg_type) {
-			case MSG_AUTH_USER:
+			case MSG_AUTH_USER_TOKEN:
 				// minimal checking, we leav it up to authenticateUser to do the real
 				// checking
 				if(!isset($msg["user"])) $msg["user"] = "";
@@ -50,19 +45,115 @@ if($pid == -1) {
 				$username = $msg["user"];
 				$passcode = $msg["passcode"];
 				global $myga;
-				msg_send($cl_queue, MSG_AUTH_USER, $myga->authenticateUser($username, $passcode));
+				msg_send($cl_queue, MSG_AUTH_USER_TOKEN, $myga->authenticateUser($username, $passcode));
 				break;
-			case MSG_ADD_USER:
+			case MSG_ADD_USER_TOKEN:
 				if(!isset($msg["username"])) {
-					msg_send($cl_queue, MSG_ADD_USER, false);	
+					msg_send($cl_queue, MSG_ADD_USER_TOKEN, false);	
 				} else {
 					$username = $msg["username"];				
 					global $myga;
-					msg_send($cl_queue, MSG_ADD_USER, $myga->setUser($username));
+					msg_send($cl_queue, MSG_ADD_USER_TOKEN, $myga->setUser($username));
 				}
 				break;
 			case MSG_DELETE_USER:
+				if(!isset($msg["username"])) {
+					msg_send($cl_queue, MSG_DELETE_USER, false);	
+				} else {
+					$username = $msg["username"];				
+					global $myga;
+					msg_send($cl_queue, MSG_DELETE_USER, $myga->deleteUser($username));
+				}
+			case MSG_AUTH_USER_PASSWORD:
+				// TODO
+				if(!isset($msg["username"])) {
+					msg_send($cl_queue, MSG_AUTH_USER_PASSWORD, false);
+					break;
+				}
+				if(!isset($msg["password"])) {
+					msg_send($cl_queue, MSG_AUTH_USER_PASSWORD, false);
+					break;
+				}
+				
+				$username = $msg["username"];
+				$sql = "select users_password from users where username='$username'";
+				$dbo = getDatabase();
+				
+				
+				// TODO now do auth
+				
 				break;
+			case MSG_SET_USER_PASSWORD:
+				// TODO
+				if(!isset($msg["username"])) {
+					msg_send($cl_queue, MSG_SET_USER_PASSWORD, false);
+					break;
+				}
+				if(!isset($msg["password"])) {
+					msg_send($cl_queue, MSG_SET_USER_PASSWORD, false);
+					break;
+				}
+				
+				// these are irrelavent yet
+				// TODO now set pass
+				break;
+			case MSG_SET_USER_REALNAME:
+				// TODO
+				if(!isset($msg["username"])) {
+					msg_send($cl_queue, MSG_SET_USER_REALNAME, false);
+					break;
+				}
+				if(!isset($msg["realname"])) {
+					msg_send($cl_queue, MSG_SET_USER_REALNAME, false);
+					break;
+				}
+				
+				$username = $msg["username"];
+				$realname = $msg["realname"];
+				$sql = "update set users_realnemd='$realname' where username='$username'";
+				$dbo = getDatabase();
+				
+				$dbo->query($sql);
+
+				msg_send($cl_queue, MSG_SET_USER_REALNAME, true);
+				
+				// TODO now set real name
+				break;
+			case MSG_SET_USER_TOKEN:
+				// TODO
+				if(!isset($msg["username"])) {
+					msg_send($cl_queue, MSG_SET_USER_TOKEN, false);
+					break;
+				}
+				if(!isset($msg["tokenstring"])) {
+					msg_send($cl_queue, MSG_SET_USER_TOKEN, false);
+					break;
+				}
+				
+				global $myga;
+				msg_send($cl_queue, MSG_SET_USER_TOKEN, $myga->setUserKey($username, $passcode));
+				
+				// TODO now set token 
+				break;			
+			case MSG_SET_USER_TOKEN_TYPE:
+				// TODO
+				if(!isset($msg["username"])) {
+					msg_send($cl_queue, MSG_SET_USER_TOKEN_TYPE, false);
+					break;
+				}
+				if(!isset($msg["tokentype"])) {
+					msg_send($cl_queue, MSG_SET_USER_TOKEN_TYPE, false);
+					break;
+				}
+				
+				$username = $msg["username"];
+				$tokentype = $msg["tokentype"];
+				global $myga;
+				msg_send($cl_queue, MSG_SET_USER_TOKEN_TYPE, $myga->setTokenType($username, $tokentype));
+				
+				// TODO now set token 
+				break;			
+				
 		}		
 	}	
 }
