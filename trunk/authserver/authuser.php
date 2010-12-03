67f928e1ecd3f81eda1311c02ed572ec1651ddae
takigama <takigama@2b1365a5-5b14-1214-0a5f-45382c3b6e0f>
2010-12-03 03:11:01 +0000

    some proof of concept work on the authserver

--- trunk/authserver/authuser.php
+++ trunk/authserver/authuser.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * radverify.php
+ * 
  * 
  * This file is designed as a "script" extension to freeradius (or some such tool) for radius authentication.
  * Also provided is a simple web interface for managing users in freeradius.
@@ -9,4 +9,25 @@
  * 
  */
 
+require_once("lib/authClient.php");
+
+$myAC = new GAAuthClient();
+
+if(!isset($argv[1])) {
+	echo "Usage: ".$argv[0]." add|auth username passcode\n";
+	return 0;	
+}
+
+switch($argv[1]) {
+	case "auth":
+		if($myAC->authUser($argv[2], $argv[3])==1) {
+			echo "Pass!";
+		} else {
+			echo "Fail!";
+		}
+		break;
+	case "add":
+		$myAC->addUser($argv[2]);
+		break;
+}
 ?>
