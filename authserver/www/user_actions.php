af54cd92490668c61e3fb88ce05662807727f61f
takigama <takigama@2b1365a5-5b14-1214-0a5f-45382c3b6e0f>
2010-12-06 07:33:11 +0000

    added user-facing site

---
+++ authserver/www/user_actions.php
@@ -0,0 +1,38 @@
+<?php 
+require_once("../lib/authClient.php");
+
+$myAC = new GAAuthClient();
+
+
+$loggedin = false;
+session_start();
+
+if(isset($_SESSION["loggedin"])) if($_SESSION["loggedin"]) {
+	error_log("session exists, poof!");
+	$loggedin = true;
+} else {
+	error_log("no session");
+	$loggedin = false;
+}
+
+if(isset($_REQUEST["action"])) {
+	error_log("action set: ".$_REQUEST["action"]);
+	switch($_REQUEST["action"]) {
+		case "login":
+			error_log("being login");
+			$username = $_REQUEST["username"];
+			$token = $_REQUEST["tokencode"];
+			
+			if($myAC->authUserToken($username, $token)) {
+				
+				$_SESSION["loggedin"] = true;
+				$_SESSION["username"] = $username;
+				header("Location: index.php");
+			} else {
+				error_log("login failed, $username, $token");
+				header("Location: index.php?message=loginfail");
+			}
+			break;
+	}
+}
+?>
